176fee4db2e606617368ecf2635ffc4b61ace58e
qwerjunety <junemelodelachica@gmail.com>
2018-08-18 09:19:01 +0800

    Initial commit

---
+++ app/src/main/java/ph/codegum/mysqlexampleproject/DbStrings.java
@@ -0,0 +1,9 @@
+package ph.codegum.mysqlexampleproject;
+
+public class DbStrings {
+
+    static final String DATABASE_URL = "10:20:30:50:3306";
+    static final String DATABASE_NAME = "mhs_example";
+    static final String USERNAME = "some_user";
+    static final String PASSWORD = "your_password";
+}
